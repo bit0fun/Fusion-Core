02765a89fa43c655ac30c36410eaf113b3ccc355
bit0fun <bit0fun@rvolve.space>
2017-02-22 11:17:07 -0500

    Added license information to existing files

--- Fusion-Core-Base/ALU.v
+++ Fusion-Core-Base/ALU.v
@@ -1,3 +1,23 @@
+/*
+  This file is part of Fusion-Core-ISA.
+
+    Fusion-Core-ISA is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Fusion-Core-ISA is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Fusion-Core-ISA.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
 module ALU(op_a, op_b, out, op_code, ar_code);
 //32 bit ALU, will upgrade to 64 bit after tests are done.
 
