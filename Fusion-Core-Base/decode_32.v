aeb9b7716491db1194acbe1288c29cd64332fa4e
bit0fun <dylanwadler@gmail.com>
2018-02-19 20:13:01 -0500

    Tentative Decode Unit Need to create testbench for decode unit, should work otherwise. All signals are generated, though unsure at the moment if will work 100% of the time. Next update should include definitive working decode unit.

--- Fusion-Core-Base/decode_32.v
+++ Fusion-Core-Base/decode_32.v
@@ -1,15 +1,21 @@
+/*** Defines  ***/
+/* Pipeline information */
+`define	PIPELINE_LENGTH  5; 
+`define PIPELINEL_2		 $clog2(PIPELINE_LENGTH);
+
+
 /*Defining opcodes to make things easier*/
-localparam  OPC_INT 	= 5'b010011;
-localparam  OPC_IMM 	= 5'b010110;
-localparam  OPC_LI		= 5'b010000;
-localparam	OPC_BRANCH	= 5'b001101;
-localparam	OPC_JMP		= 5'b001100;
-localparam 	OPC_JLNK	= 5'b000100;
-localparam	OPC_LD		= 5'b011110;
-localparam	OPC_ST		= 5'b011101;
+`define  OPC_INT 	 5'b010011;
+`define  OPC_IMM 	 5'b010110;
+`define  OPC_LI		 5'b010000;
+`define  OPC_BRANCH	 5'b001101;
+`define	 OPC_JMP	 5'b001100;
+`define	 OPC_JLNK	 5'b000100;
+`define	 OPC_LD		 5'b011110;
+`define  OPC_ST		 5'b011101;
 
 /*System because it's special*/
-localparam	OPC_SYS		= 5'b110000;
+`define	OPC_SYS		 5'b110000;
 
 module decode_32(
 	input[31:0] 	insn_in,			//input instruction
@@ -29,7 +35,7 @@ module decode_32(
 											//size
 
 	/*Control Signals*/
-	output[4:0] 	reg aluop_out,
+	output[3:0] 	reg aluop_out,
 	output			reg jlnk_out,
 	output			reg pc_change_rel_out,
 	output			reg pc_change_abs_out,
@@ -39,12 +45,17 @@ module decode_32(
 	/*Flag outputs*/
 	output			reg nri_flg_out,		//output of 'not real instruction' flag
 	output[1:0]		reg branch_funct_out,	//branch function; taken from the funct field
+	output			reg stall_out,
 
 	/*System instruction signals*/
 	output			reg memsync_out,
 	output			reg syscall_out,
 	output			reg pem_inc_req_out,
 	output			reg pem_dec_req_out,
+	output[7:0]		reg sysreg_addr_out,	//for reading and writing to system registers
+	output[7:0]		reg sysreg_data_out,
+	input[7:0]			sysreg_data_in,
+
 
 	/* Misc. Outputs*/
 	output[31:0]	reg insn_pc_out,
@@ -85,12 +96,14 @@ module decode_32(
 	wire		pc_change_w;			//instruction can change the pc
 	wire		mem_access_w;
 	wire		pc_link_w;				//jump that requires linking to ra
+	wire		sys_insn_w;
 
 	/*secondary calculated resource usage*/
 	wire		mem_read_req_w;
 	wire		mem_write_req_w;
 	wire		pc_abs_w;
 	wire		pc_rel_w;
+	wire		memsync_w;
 	
 	/*ALU op signal selection*/
 	wire		aluop_intsig_w; // integer/register operation field used
@@ -131,6 +144,8 @@ module decode_32(
 	assign	pc_change_w		= ~opcode_w[4];				
 	assign	mem_access_w	= opcode_w[4] & opcode_w[3];
 	assign	pc_link_w		= opcode_w[4] ~| opcode_w[3];				
+	assign	sys_insn_w		= is_cp_insn_w & opcode_w[4];
+	assign 	memsync_w = (sys_insn_w && ( (funct_sys_w & 8'h04) || (funct_sys_w & 8'b0 ) ); //if sync or syscall, need to sync memory 
 
 	assign	use_rd_w		= (opcode_w[1] | opcode_w[0]) & (~opcode_w[2] | opcode_w[1]);
 	assign	use_rsa_w		= (opcode_w[2] | opcode_w[1]);
@@ -151,9 +166,14 @@ module decode_32(
 	assign  pc_rel_w		= (pc_change & 	( (opcode[2:0] && 3'b101)  || (rsa_addr_w == (5'b0)) ) ); //not jr/jrl or branch
 	assign	pc_abs_w		= (pc_change & ~( (opcode[2:0] && 3'b101)  || (rsa_addr_w == (5'b0)) ) );
 
+/* Internal registers*/
+	reg	[PIPELINEL_2 - 1 : 0]	memstall_count; //counter for stalls in pipeline
+
+
 /*** LOGIC SECTION ****/
 
-always@(~reset_in) begin //while in reset
+always@(~reset_in or stall_in) begin //while in reset or stalling
+
 	reg_select_out 		<= 5'b0;			//output register bank select
 	rsa_out 			<= 5'b0;			//output of register a address
 	rsb_out 			<= 5'b0;			//output of register b address
@@ -176,6 +196,8 @@ always@(~reset_in) begin //while in reset
 	syscall_out			<= 1'b0;
 	pem_inc_req_out		<= 1'b0;
 	pem_dec_req_out		<= 1'b0;
+	sysreg_addr_out		<= 8'b0;
+	sysreg_data_out		<= 8'b0;
 
 	insn_pc_out			<= 32'h0; 
 	cp_insn_out			<= 32'h0;
@@ -183,7 +205,7 @@ always@(~reset_in) begin //while in reset
 end
 
 
-always@(posedge clk or negedge reset_in) begin
+always@(posedge clk and reset_in and ~stall_in) begin //make sure that reset has to be high for this to work
 	/*** Determining ALUOP value ***/
 	if(aluop_intsig_w) begin //uses ALUOP field
 		aluop_out <= aluop_w;
@@ -196,7 +218,7 @@ always@(posedge clk or negedge reset_in) begin
 	end
 
 	/*Branch funct assignment*/
-	branch_funct_out <= funct_b_w;
+	branch_funct_out <= (aluop_bsig_w) ? funct_b_w: 2'b0;
 
 	/*Co-processor instruction output to co-processor decode*/
  	cp_insn_out 	<= (is_cp_insn_w) ? insn_in[30:0] : 30'b0; 	//if is a co-processor instruction, output instruction to
@@ -208,7 +230,14 @@ always@(posedge clk or negedge reset_in) begin
  	pc_change_rel_out <= pc_rel_w;
 	pc_change_abs_out <= pc_abs_w;
 
-	//aluop_out	<= ( use_alu_w ) ? aluop_w : 4'b1111; //alu nop
+	/*Register selection and outputs*/
+	if(imm_sel_w == `OPC_LI) begin //determine which register bank to use, from DSEL
+		reg_select_out	<= { 1'b0, dsel_li_w}; //lower 2 bits determine file, upper two bits determine signed, upper or lower 16 bits
+	end else if(sys_insn_w && ( funct_sys_w & (8'h02 )) begin //if system instruction, need to check if accessing system registers, last two bits matter here
+		reg_select_out	<= 5'b10000; //upper most bit is for accessing special purpose registers; maps to memory address to make 8 bit registers easier
+	end else //everything else should just use general purpose register file
+		reg_select_out	<= 5'b0;
+	end
 
 	rsa_out <= (use_rsa_w) ? rsa_addr_w : 5'b0;
 	rsb_out <= (use_rsb_w) ? rsb_addr_w : 5'b0;
@@ -219,13 +248,14 @@ always@(posedge clk or negedge reset_in) begin
 	/*** Selecting immediate values ***/
 	if(!is_cp_insn_w) begin
 		case (imm_sel_w) //figure out what immediate value to use
-			OPC_IMM:	imm_out <= {9'b0 ,imm_i_w};
-			OPC_LD:		imm_out <= {7'b0, imm_ld_w};
-			OPC_ST:		imm_out <= {7'b0, imm_st_w};
-			OPC_LI:		imm_out <= {6'b0, imm_li_w};
-			OPC_JMP:	imm_out <= imm_j_w;
-			OPC_JLNK:	imm_out <= imm_j_w;
-			OPC_BRANCH:	imm_out <= {7'b0, imm_b_w};
+			`OPC_IMM:		imm_out <= {9'b0 ,imm_i_w};
+			`OPC_LD:		imm_out <= {7'b0, imm_ld_w};
+			`OPC_ST:		imm_out <= {7'b0, imm_st_w};
+			`OPC_LI:		imm_out <= {6'b0, imm_li_w};
+			`OPC_JMP:		imm_out <= imm_j_w;
+			`OPC_JLNK:		imm_out <= imm_j_w;
+			`OPC_BRANCH:	imm_out <= {7'b0, imm_b_w};
+			default:		imm_out <= 21'b0;	//this may be redundant or cause errors, not sure yet
 		endcase
 		/*co processor instruction immediates; right now just system insn due to direct
 		* affects on main core pipeline */
@@ -238,28 +268,43 @@ always@(posedge clk or negedge reset_in) begin
 
 
 	/*System decoding*/
-	case (funct_sys_w)
-		8'h00: begin				//system call
-			syscall_out 	<= 1'b1;
-			memsync			<= 1'b0;
-			pem_inc_req_out	<= 1'b0;
-			pem_dec_req_out	<= 1'b0;
-		end
-		8'h01: begin				//system return
-		end
-		8'h04: begin				//memory sync
-			syscall_out 	<= 1'b0;
-			memsync			<= 1'b1;
-			pem_inc_req_out	<= 1'b0;
-			pem_dec_req_out	<= 1'b0;
+	if( sys_insn_w) begin //if actually a system instruction
+		case (funct_sys_w)
+			8'h00: begin				//system call
+				syscall_out 	<= 1'b1;
+				memsync_out		<= 1'b1; //need to also syncronize memory first
+				pem_inc_req_out	<= 1'b0;
+				pem_dec_req_out	<= 1'b0;
+			end
+		//	8'h01: begin				//system return
+		//	end
+			8'h04: begin				//memory sync
+				syscall_out 	<= 1'b0;
+				memsync_out		<= 1'b1;
+				pem_inc_req_out	<= 1'b0;
+				pem_dec_req_out	<= 1'b0;
+				stall_out		<= 1'b1;
 
+			end
+			default: begin
+				syscall_out 	<= 1'b0;
+				memsync_out		<= 1'b0;
+				pem_inc_req_out	<= 1'b0;
+				pem_dec_req_out	<= 1'b0;
+			end
+		endcase
+	end
+
+	/* Ensure stall_out from memory sync doesn't make the pipeline get stuck */
+	if(memsync_w) begin
+		if(memstall_count > `PIPELINE_LENGTH) begin
+			memstall_count 	<= 0;
+			memsync_out		<= 1'b0;
 		end
-		default: begin
-			syscall_out 	<= 1'b0;
-			memsync			<= 1'b0;
-			pem_inc_req_out	<= 1'b0;
-			pem_dec_req_out	<= 1'b0;
+		else begin
+			memstall_count = memstall_count + 1;
 		end
+	end
 
 
 end
